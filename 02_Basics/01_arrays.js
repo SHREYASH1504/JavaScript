b87d5234b2fff7d0876939721386cf8aba731792
Shreyash Nikhare <shreyashnikhare1504@gmail.com>
2025-08-30 17:48:43 +0530

    Refactor array methods examples and add new array manipulation techniques

--- 02_Basics/01_arrays.js
+++ 02_Basics/01_arrays.js
@@ -18,9 +18,38 @@ const newArr2 = myArr.join();
 
 // slice and splice
 const myn1 = myArr.slice(1, 4); // does not modify original array
-console.log("A ", myArr); // 0, 1, 2, 3, 4
-console.log("B ", myn1); // 1, 2, 3
+// console.log("A ", myArr); // 0, 1, 2, 3, 4
+// console.log("B ", myn1); // 1, 2, 3
 
 const myn2 = myArr.splice(1, 4); // modifies original array
-console.log("C ", myArr); // 0
-console.log("D ", myn2); // 1, 2, 3, 4
+// console.log("C ", myArr); // 0
+// console.log("D ", myn2); // 1, 2, 3, 4
+
+// ************************************* PART 2 *****************************************
+
+// const marvel_heros = ["Iron Man", "Captain America", "Thor", "Hulk", "Black Widow"];
+// const dc_heros = ["Superman", "Batman", "Wonder Woman", "Flash", "Aquaman"];
+
+// marvel_heros.push(dc_heros);
+// console.log(marvel_heros);
+
+// const allHeros = marvel_heros.concat(dc_heros);
+// console.log(allHeros);
+
+// spread
+// const all_new_heros = [...marvel_heros, ...dc_heros];
+// console.log(all_new_heros);
+
+// const mix_array = [1, 2, 3, [4 , 5, 6], 7, [1, 2, [4, 5, 1]]];
+// const real_mix_array = mix_array.flat(Infinity);
+// console.log(real_mix_array);
+
+// console.log(Array.isArray("Shreyash"));
+// console.log(Array.from("Shreyash"));
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+console.log(Array.of(score1, score2, score3)); // [ 100, 200, 300 ]
+
--- 02_Basics/tempCodeRunnerFile.js
+++ 02_Basics/tempCodeRunnerFile.js
@@ -1,8 +1,2 @@
-); // 5
-// console.log(myArr.push(5)); // Adds 5 to the end
-// console.log(myArr.pop()); // Removes last element
-// console.log(myArr.shift()); // Removes first element
-// console.log(myArr.unshift(0)); // Adds 0 to the beginning
-
-// console.log(myArr.includes(8));
-// console.log(myArr.indexOf(3));
+ 3, 4
+// console.log("B ", myn1); // 1, 2, 3
